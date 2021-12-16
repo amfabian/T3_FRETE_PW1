4d2bbd5275f8cd92fa529cb9d76ffd6a77ecc4ea
Alexandre Fabian <xandy.fabian@gmail.com>
2021-12-16 17:09:02 -0300

    Criado testes/Teste.java

--- src/main/java/testes/Teste.java
+++ src/main/java/testes/Teste.java
@@ -1,5 +1,23 @@
 package testes;
 
+import classes.inheritance.joined.Cliente;
+import dao.ClienteDAO;
+
 public class Teste {
+	public static void main(String[] args) {
+		Cliente c1  = new Cliente();
+		c1.setCpf("01235488001");
+		c1.setEndereco("Rua Vasco Alves");
+		c1.setNome("Alexandre");
+		c1.setTelefone("51999403450");
+		ClienteDAO objDAO = new ClienteDAO();
+		if(objDAO.cadastrar(c1)) {
+			System.out.println("Cliente inserido com sucesso!");
+		} else {
+			System.out.println("Erro ao inserir o cliente!");
+		}
+	
+		
+	}
 
 }
