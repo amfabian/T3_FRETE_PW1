eee3815dfda45eb284e2c9af5e92390e1c15bfc9
Alexandre Fabian <xandy.fabian@gmail.com>
2021-12-16 16:54:40 -0300

    Implementado ClienteDAO

--- src/main/java/dao/ClienteDAO.java
+++ src/main/java/dao/ClienteDAO.java
@@ -5,20 +5,16 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 
-import classes.inheritance.joined.Pessoa;
+import classes.inheritance.joined.Cliente;
 import util.JPAUtil;
 
-
-public class PessoaDAO {
+public class ClienteDAO {
 
 	private EntityManager em;
-
-	public PessoaDAO() {
-	}
 	
+	public ClienteDAO() {};
 	
-	
-	public boolean cadastrar(Pessoa entity) {
+	public boolean cadastrar(Cliente entity) {
 		try {
 			em = JPAUtil.getEntityManager();
 			em.getTransaction().begin();
@@ -27,15 +23,14 @@ public class PessoaDAO {
 			return true;
 		} catch (RuntimeException e) {
 			if (em.getTransaction().isActive()) {
-				System.out.println("\nErro ao cadastrar nova Pessoa. Revertendo alteracoes");
+				System.out.println("\nErro ao cadastrar novo Cliente. Revertendo alteracoes");
 				em.getTransaction().rollback();
 			}
 			return false;
 		}
 	}
 
-	
-	public boolean atualizar(Pessoa entity) {
+	public boolean atualizar(Cliente entity) {
 		try {
 			em = JPAUtil.getEntityManager();
 			em.getTransaction().begin();
@@ -44,70 +39,68 @@ public class PessoaDAO {
 			return true;
 		} catch (RuntimeException e) {
 			if (em.getTransaction().isActive()) {
-				System.out.println("\nErro ao atualizar a pessoa. Revertendo alteracoes");
+				System.out.println("\nErro ao atualizar o Cliente\n" + entity + "\nRevertendo alteracoes");
 				em.getTransaction().rollback();
 			}
 			return false;
 		} 
 	}
 
-	
 	public boolean excluir(long id) {
 		try {
 			em = JPAUtil.getEntityManager();
 			em.getTransaction().begin();
-			Pessoa entity = em.find(Pessoa.class, id);
+			Cliente entity = em.find(Cliente.class, id);
 			em.remove(entity);
 			em.getTransaction().commit();
 			return true;
 		} catch (RuntimeException e) {
 			if (em.getTransaction().isActive()) {
-				System.out.println("\nErro ao excluir a pessoa. Revertendo alteracoes");
+				System.out.println("\nErro ao excluir o cliente com id " + id + "\nRevertendo alteracoes");
 				em.getTransaction().rollback();
 			}
 			return false;
 		} 
 	}
-	
 
 	
-	public List<Pessoa> listarTodos() {
+	public List<Cliente> listarTodos() {
 		try {
 			em = JPAUtil.getEntityManager();
-			TypedQuery<Pessoa> query = em.createQuery("SELECT obj FROM Pessoa obj", Pessoa.class);
-			List<Pessoa> pessoas = query.getResultList();
-			return pessoas;
+			TypedQuery<Cliente> query = em.createQuery("SELECT obj FROM Cliente obj", Cliente.class);
+			List<Cliente> clientes = query.getResultList();
+			return clientes;
 		} catch (RuntimeException e) {
-			System.out.println("\nErro ao listar todas as pessoa. Imprimindo erro:");
+			System.out.println("\nErro ao listar todas os clientes. \nImprimindo erro:\n");
 			e.printStackTrace();
 			return null;
 		} 
 	}
-
 	
-	public Pessoa pesquisarKey(long id) {
+	public Cliente pesquisarKey(long id) {
 		try {
 			em = JPAUtil.getEntityManager();
-			Pessoa entity = em.find(Pessoa.class, id);
+			Cliente entity = em.find(Cliente.class, id);
 			return entity;
 		} catch (RuntimeException e) {
 			if (em.getTransaction().isActive()) {
-				System.out.println("\nErro ao pesquisar pelo id da pessoa");
+				System.out.println("\nErro ao pesquisar pelo id do cliente");
 				em.getTransaction().rollback();
 			}
 			return null;
 		} 
 	}
 
+
 	
-	public Pessoa pesquisarNome(String nome) {
+	public Cliente pesquisarCPF(String cpf) {
 		try {
 			em = JPAUtil.getEntityManager();
-			Pessoa entity = em.find(Pessoa.class, nome);
+			Cliente entity = em.find(Cliente.class, cpf);
 			return entity;
 		} catch (RuntimeException e) {
 			if (em.getTransaction().isActive()) {
-				System.out.println("\nErro ao pesquisar pelo nome da pessoa");
+				System.out.println("\nErro ao pesquisar pelo cpf do cliente");
 				em.getTransaction().rollback();
 			}
 			return null;
